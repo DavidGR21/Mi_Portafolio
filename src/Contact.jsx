a35decf9708221b2e04a161a94323ddd1df77ab0
David Giler Rizzo <davidgiler21@gmail.com>
2024-11-23 11:00:14 -0500

    Diseño de la pestaña Contacto completo

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,18 +2,21 @@ import "./Contact.css";
 function Info() {
 
     return (
-        <div className="container_Info">
+        <div className="container_Contact">
+            <h2 className="titulo_Info">CONTACT</h2>
             <div className="formContainer">
-                <button type="submit">Send</button>
+                <input required id="email" placeholder="example@email.com" type="email" />
+                <label htmlFor="email">Email</label>
+                <input id="name" placeholder="Your name" type="text" />  
+                <label htmlFor="name">Name</label>
                 <textarea id="message" placeholder="Write your message" rows="5"></textarea>
                 <label htmlFor="message">Message</label>
-                <input id="name" placeholder="Your name" type="text" />
-                <label htmlFor="name">Name</label>
-                <input id="email" placeholder="example@email.com" type="email" />
-                <label htmlFor="email">Email</label>
+                <div className="social-buttons">
+                    <a >SUBMIT</a>
+                </div>
             </div>
         </div>
-    );
+    );                                                                                                                                                                            
 }
 
 export default Info;
