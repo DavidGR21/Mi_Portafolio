c8b9729c3bc2626481da4dc7834ee04afe958933
David Giler Rizzo <davidgiler21@gmail.com>
2024-11-23 21:35:10 -0500

    Diseño de la imagen en grande de la Presentacion de Proyectos

--- src/Works.jsx
+++ src/Works.jsx
@@ -3,19 +3,21 @@ import { useState } from 'react';
 
 function Works() {
     const listworks = [
-        { id: '01', nombre: "WORK1", url: "https://cdn.sanity.io/images/7b0p5ml9/production/28c54dc21c037d45d1aaf3cae45db267d1167e23-3830x2144.png?auto=format&q=80&w=1920", descripcion: "descripcion de la imagen 1" },
-        { id: '02', nombre: "WORK2", url: "https://cdn.sanity.io/images/7b0p5ml9/production/6b5fe1f056531e95e933afe8e06555ad6f18e953-2000x2000.png?auto=format&q=80&w=1920", descripcion: "descripcion de la imagen 2" },
-        { id: '03', nombre: "WORK3", url: "https://cdn.sanity.io/images/7b0p5ml9/production/28c54dc21c037d45d1aaf3cae45db267d1167e23-3830x2144.png?auto=format&q=80&w=1920", descripcion: "descripcion de la imagen 3" },
-        { id: '04', nombre: "WORK4", url: "https://cdn.sanity.io/images/7b0p5ml9/production/28c54dc21c037d45d1aaf3cae45db267d1167e23-3830x2144.png?auto=format&q=80&w=1920", descripcion: "descripcion de la imagen 4" },
+        { id: '01', nombre: "Proyecto Mascota Virtual", url: "https://cdn.sanity.io/images/7b0p5ml9/production/28c54dc21c037d45d1aaf3cae45db267d1167e23-3830x2144.png?auto=format&q=80&w=1920", descripcion: "descripcion de la imagen 1", url_go: '' },
+        { id: '02', nombre: "WORK2", url: "https://cdn.sanity.io/images/7b0p5ml9/production/6b5fe1f056531e95e933afe8e06555ad6f18e953-2000x2000.png?auto=format&q=80&w=1920", descripcion: "descripcion de la imagen 2", url_go: '' },
+        { id: '03', nombre: "WORK3", url: "https://cdn.sanity.io/images/7b0p5ml9/production/28c54dc21c037d45d1aaf3cae45db267d1167e23-3830x2144.png?auto=format&q=80&w=1920", descripcion: "descripcion de la imagen 3", url_go: '' },
+        { id: '04', nombre: "WORK4", url: "https://cdn.sanity.io/images/7b0p5ml9/production/28c54dc21c037d45d1aaf3cae45db267d1167e23-3830x2144.png?auto=format&q=80&w=1920", descripcion: "descripcion de la imagen 4", url_go: '' },
         // Agrega más elementos si es necesario
     ];
 
     const [selectedImage, setSelectedImage] = useState('https://cdn.sanity.io/images/7b0p5ml9/production/28c54dc21c037d45d1aaf3cae45db267d1167e23-3830x2144.png?auto=format&q=80&w=1920');
     const [selectedName, setSelectedName] = useState('WORK1');
+    const [selectedURL, setSelectedURL] = useState('WORK1');
 
-    const handleMouseEnter = (url, name) => {
+    const handleMouseEnter = (url, name, url_go) => {
         setSelectedImage(url);
         setSelectedName(name);
+        setSelectedURL(url_go);
     };
 
     return (
@@ -26,7 +28,7 @@ function Works() {
                         <li
                             key={work.id}
                             className="work-item"
-                            onMouseEnter={() => handleMouseEnter(work.url, work.nombre)}
+                            onMouseEnter={() => handleMouseEnter(work.url, work.nombre, work.url_go)}
                         >
                             <div className="cont_Project">
                                 <p className="etiqueta">#{work.id}</p>
@@ -44,6 +46,7 @@ function Works() {
                 <div className="container_Img_Big">
                     <p className="name_Big">{selectedName}</p>
                     <img src={selectedImage}></img>
+                    <a className="urlGO" href={selectedURL}>GITHUB</a>
                 </div>
             </div>
         </div>
