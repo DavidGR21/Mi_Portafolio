a66a7922db6a101a9b0fb0280646eceece71a724
David Giler Rizzo <davidgiler21@gmail.com>
2024-11-24 12:50:48 -0500

    Se quito contacto debido a falta de presupuesto

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,43 +6,10 @@ function Info() {
     const [name, setName] = useState('');
     const [message, setMessage] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append('email', email);
-        formData.append('name', name);
-        formData.append('message', message);
-
-        fetch('https://formspree.io/f/xldewwbl', {
-            method: 'POST',
-            body: formData,
-        })
-            .then(response => {
-                if (!response.ok) {
-                    // Si la respuesta no es OK (status no 2xx)
-                    console.error('Error en la respuesta:', response.status);
-                    alert('Hubo un error al enviar el mensaje');
-                } else {
-                    // Si la respuesta es exitosa (status 2xx)
-                    return response.json(); // Suponiendo que la respuesta es JSON
-                }
-            })
-            .then(data => {
-                // Solo se ejecutará si la respuesta fue exitosa
-                console.log('Respuesta exitosa:', data);
-                alert('Mensaje enviado con éxito!');
-            })
-            .catch(error => {
-                // Aquí manejamos cualquier error que pueda ocurrir
-                console.error('Error al enviar el formulario:', error);
-                alert('Hubo un error al enviar el mensaje');
-            });
-    };
-
     return (
         <div className="container_Contact">
             <h2 className="titulo_Info">CONTACT</h2>
-            <form className="formContainer" onSubmit={handleSubmit}>
+            <form className="formContainer">
                 <input
                     required
                     id="email"
