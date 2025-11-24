6d326c74589acef801af0396df77cb110f2ae2d7
DavidGR21 <davidgiler21@gmail.com>
2025-11-24 12:13:52 -0500

    Añadido componente Projects con estructura básica y estilos para la presentación de proyectos.

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+import '../styles/Projects.css';
+
+function Projects() {
+    return (
+        <section id="projects">
+            <div className="projects-container">
+                PROJECTS
+            </div>
+        </section>
+    );
+}
+
+export default Projects;
