ebc3aa6992a95f7b223360f4eb61419351cb6b25
DavidGR21 <davidgiler21@gmail.com>
2025-11-24 12:14:18 -0500

    Añadido componente Contact con estructura básica y estilos para la sección de contacto.

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import '../styles/Contact.css';
+
+function Contact() {
+    return (
+        <section id="contact">
+            <div className="contact-container">
+                CONTACT
+            </div>
+        </section>
+    );
+}
+
+export default Contact;
